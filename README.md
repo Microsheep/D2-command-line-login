34e86dcb209aeee56a1d5ec3fccdb7ac3b4210ba
microsheep <twmicrosheep@gmail.com>
2015-07-17 15:48:48 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+<h2>This is D2 command line login</h2>
+
+<h5>By: Microsheep with help from kevchentw and Tocknicsu</h5>
+
+交大網路reset後，會需要d2帳號登入 (80 port)<br>
+如果是server就會很麻煩了...<br>
+下面是一支可以直接從terminal執行的登入程式<br>
+安裝python3跟beautifulsoup4就可以了~<br>
+之後直接執行 python3 login.py<br>
+輸入自己的帳號&密碼，就可以登入囉。<br>
+分享給大家 XD<br>
+
---
+++ login.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+import getpass
+
+### input account ###
+account_name=""
+account_password=""
+account_name = input("Enter username ___@nctu.edu.tw: ")
+account_name = getpass.getpass("Enter Password: ")
+### get magic ###
+res = requests.get("http://www.google.com")
+raw_data = BeautifulSoup(res.text, "lxml")
+reply = raw_data.find_all('input')[0]["value"]
+### make payload ###
+payload = {'magic': reply,'username': account_name+'@nctu.edu.tw', 'password': account_password, '4Tredir': "/"}
+### post payload ###
+r = requests.post("http://www.google.com", data=payload)
+print("Payload sent! XD")
+### check if success ###
+cres = requests.get("http://www.google.com")
+craw_data = BeautifulSoup(cres.text, "lxml")
+creply = raw_data.find_all('input')[0]["value"]
+if creply=="Big5":
+    print("Success!")
+else:
+    print("Failed with unknown reason!")
+
